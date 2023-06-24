d30a97f1e6432c3a9eb05d1a0c6d7773f5964f2b
jose.brandao <jose.brandao@americanas.io>
2023-06-24 00:15:31 -0300

    Resolver merge-conflict-final

--- src/main/java/com/example/springboot/service/dto/CarDTO.java
+++ src/main/java/com/example/springboot/service/dto/CarDTO.java
@@ -1,25 +1,13 @@
 package com.example.springboot.service.dto;
 
-<<<<<<< HEAD
+
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
-=======
-public record CarDTO (Long id, String plate, String color) {
->>>>>>> 7a05fe391fd540edabef1c292d7939ef72991cf7
 
-import org.springframework.data.annotation.CreatedDate;
-
-import com.example.springboot.service.business.Ticket;
-import com.fasterxml.jackson.annotation.JsonFormat;
-
-import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 
 
