6c5e14a53840ec029b04c4430b75862273edc9e0
jose.brandao <jose.brandao@americanas.io>
2023-06-26 20:50:29 -0300

    Update pom.xml Swagger config

--- src/main/java/com/example/springboot/Startup.java
+++ src/main/java/com/example/springboot/Startup.java
@@ -8,7 +8,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
-@EnableSwagger2
 public class Startup {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/springboot/config/SpringFoxConfig.java
+++ src/main/java/com/example/springboot/config/SpringFoxConfig.java
@@ -2,9 +2,11 @@ package com.example.springboot.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -16,6 +18,19 @@ public class SpringFoxConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
+                .build();
+    }
+    private ApiInfo apiInfo () {
+        return  new ApiInfoBuilder()
+                .title("API")
+                .description("Service API - Parking System")
+                .termsOfServiceUrl("http://springfox.io")
+                .contact(new Contact("springfox",
+                                     "https://github.com/josepaulobrandao/parking-system",
+                                     "jose.dev2301@gmailcom"))
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://github.com/josepaulobrandao/parking-system")
+                .version("2.0")
                 .build();
     }
 }
