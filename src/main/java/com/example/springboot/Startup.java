de03be55b6c16f00d3c85332fc19889ebd58e24f
jose.brandao <jose.brandao@americanas.io>
2023-07-11 19:10:58 -0300

    Fix:swagger config

--- src/main/java/com/example/springboot/Startup.java
+++ src/main/java/com/example/springboot/Startup.java
@@ -3,9 +3,27 @@ package com.example.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
 
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Parking System API",
+description = "API para gerenciamento de estacionamento",
+version = "1.0",
+contact = @Contact(
+        name = "José Paulo",
+        email = "jose.dev2301@gmail.com",
+        url = "https://github.com/josepaulobrandao/parking-system"
+),
+license = @License(
+        name = "springdoc.org",
+        url = "https://springdoc.org/"
+)
+))
 public class Startup {
 
 	public static void main(String[] args) {
